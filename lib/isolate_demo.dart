a8c6f26c8cbcde2f28be7b22caab529cf40161d3
conghaonet <hao.cong@app2m.com>
2019-12-12 21:45:01 +0800

    add: isolate_demo.dart

--- lib/isolate_demo.dart
+++ lib/isolate_demo.dart
@@ -1,6 +1,6 @@
 import 'dart:isolate';
 
-void main() async {
+void main(List<String> args) async {
   // 创建管道
   ReceivePort receivePort = ReceivePort();
 
@@ -9,16 +9,20 @@ void main() async {
 
   // 监听管道消息
   receivePort.listen((data) {
-    print('Data：$data');
-
-    // 不再使用时，我们会关闭管道
-    receivePort.close();
-
+    print('>>>>Data：$data');
+    if((data as String).contains('!')) {
+      // 不再使用时，我们会关闭管道
+      receivePort.close();
+    }
+  }, onDone: () {
+    print('>>>>onDone');
     // 需要将isolate杀死
     isolate?.kill(priority: Isolate.immediate);
   });
 }
 
 void foo(SendPort sendPort) {
+//  sleep(Duration(seconds: 3));
   sendPort.send("Hello World");
+  sendPort.send("Hello World!!");
 }
